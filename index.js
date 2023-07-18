6b13885c2565dfc39cab1e9a226c4d5f3dbd700b
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2023-07-18 13:22:43 +0100

    commit

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const sideNav = document.querySelector('.side-nav-div');
 const appGrid = document.getElementById('app-grid')
+const viewMoreBtn = document.getElementById('view-more')
+const hiddenStories = document.querySelectorAll('.hid')
 
 appGrid.addEventListener('click', function() {
     sideNav.classList.toggle('side-nav-div');
     
+})
+
+viewMoreBtn.addEventListener('click', function() {
+    for(story of hiddenStories) {
+        console.log(story)
+        story.classList.remove('hid')
+    }
 })
